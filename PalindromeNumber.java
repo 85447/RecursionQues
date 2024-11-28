ba35a43bbd4a07362c0dafb7dbcb6fee613effb4
Siya_kalia <kaliasiya007@gmail.com>
2024-11-28 11:42:05 +0530

    questions

---
+++ PalindromeNumber.java
@@ -0,0 +1,28 @@
+public class PalindromeNumber {
+    static int rev(int n){
+        //somethimes you might need some additional variables in the argument
+        // in that case make another helper function
+        int digits = (int)(Math.log10(n) + 1);
+        return helper(n,digits);
+    }
+
+    private static int helper(int n, int digits) {
+        if(n%10 == n){
+            return n;
+        }
+        int rem = n % 10;
+        return (int) (rem * Math.pow(10, digits-1) + helper(n/10,digits-1));
+    }
+
+    static boolean palin(int n){
+        return n == rev(n);
+    }
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println(palin(1234321));
+
+    }
+}
